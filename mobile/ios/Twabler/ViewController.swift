5a6d36d32e98a4f0638335362a695ab54f5325ae
Ariel Takvam <ariel@takvam.net>
2019-04-10 20:51:53 -0700

    added properties

--- mobile/ios/Twabler/ViewController.swift
+++ mobile/ios/Twabler/ViewController.swift
@@ -10,7 +10,31 @@ import UIKit
 
 class ViewController: UIViewController {
     
-
+    // MARK: Properties
+    
+    @IBOutlet weak var TweetImageViewContainer: UIView!
+    @IBOutlet weak var TweetImageView: UIImageView!
+    
+    @IBOutlet weak var TweetTableView: UITableView!
+    @IBOutlet weak var TweetTableViewCell: UITableViewCell!
+    
+    @IBOutlet weak var TabBar: UITabBar!
+    @IBOutlet weak var TabBarHomeButton: UITabBarItem!
+    @IBOutlet weak var TabBarAboutButton: UITabBarItem!
+    @IBOutlet weak var TabBarSettingsButton: UITabBarItem!
+    
+    @IBOutlet weak var BlurView: UIView!
+    
+    
+    
+    
+    @IBAction func ForgotPasswordButton(_ sender: Any) {
+    }
+    
+    @IBAction func SignUpButton(_ sender: Any) {
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
